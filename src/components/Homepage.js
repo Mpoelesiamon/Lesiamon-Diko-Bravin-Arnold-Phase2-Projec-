4d006ea149c1a0604c9f4aa9ac9b93403936793e
matchy123 <user.email>
2023-11-22 11:48:40 +0300

    Done

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h2>Welcome to the React Shopping App!</h2>
+      <p>
+        Explore a variety of items in different categories. Click on each item to view details. Use the navigation bar to switch between categories and check
+        your shopping cart.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
