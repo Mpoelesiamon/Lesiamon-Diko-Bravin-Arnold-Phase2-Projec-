cb2f9dad1f372e9d51113f4dc761980a4d5a593d
Bravin <onsasebravin853@gmail.com>
2023-11-22 14:27:33 +0300

    Category

---
+++ src/components/Category.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Category ({ category, data, addToCart }) {
+
+    const [ likes, setLikes] = useState({});
+
+    const handleLike = (itemId) => {
+        setLikes((prevLikes) => ({
+            ...prevLikes,
+            [itemId] : (prevLikes[itemId] || 0) + 1,
+        }));
+    };
+
+    return (
+        <div>
+            <h2>{category}</h2>
+            <ul>
+                {Object.entries(data).map(([itemId, item]) => (
+                    <li key={itemId}>
+                        <div>
+                            <img src={item.imgUrl} alt={item.name} />
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
