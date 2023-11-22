34d23eee9b97d228ab2155c5f427fe67d551ae2f
Lesiamon <lesiamon.mpoe@student.moringaschool.com>
2023-11-22 12:42:15 +0300

    Done

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,28 +1,38 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function Category ({ category, data, addToCart }) {
+function Category({ category, data, addToCart }) {
 
-    const [ likes, setLikes] = useState({});
+  const [likes, setLikes] = useState({});
 
-    const handleLike = (itemId) => {
-        setLikes((prevLikes) => ({
-            ...prevLikes,
-            [itemId] : (prevLikes[itemId] || 0) + 1,
-        }));
-    };
+  const handleLike = (itemId) => {
+    setLikes((prevLikes) => ({
+      ...prevLikes,
+      [itemId]: (prevLikes[itemId] || 0) + 1,
+    }));
+  };
 
-    return (
-        <div>
-            <h2>{category}</h2>
-            <ul>
-                {Object.entries(data).map(([itemId, item]) => (
-                    <li key={itemId}>
-                        <div>
-                            <img src={item.imgUrl} alt={item.name} />
-                        </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div>
+      <h2>{category}</h2>
+      <ul>
+        {Object.entries(data).map(([itemId, item]) => (
+          <li key={itemId}>
+            <div>
+              <img src={item.imgUrl} alt={item.name} />
+            </div>
+            <div>
+              <p>Name: {item.name}</p>
+              <p>Price: Ksh{item.price}</p>
+              <p>Details: {item.details}</p>
+              <button onClick={() => addToCart({ id: itemId, ...item })}>Add to Cart</button>
+              <button onClick={() => handleLike(itemId)}>Like</button>
+              <p>Likes: {likes[itemId] || 0}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default Category;
