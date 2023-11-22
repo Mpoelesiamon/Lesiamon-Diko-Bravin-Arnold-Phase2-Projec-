977d7a0a66062cf265f2b2c68996e3d00620c264
matchy123 <user.email>
2023-11-22 12:09:24 +0300

    Done

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/electronic">Electronic</Link>
+        </li>
+        <li>
+          <Link to="/fashion">Fashion</Link>
+        </li>
+        <li>
+          <Link to="/sports">Sports</Link>
+        </li>
+        <li>
+          <Link to="/gaming">Gaming</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
