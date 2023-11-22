fd7b610d619c537f51faf6c7a7ccaef26842b3c4
Bravin <onsasebravin853@gmail.com>
2023-11-22 15:01:46 +0300

    Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function Category ({ category, data, addToCart }) {
+function Category({ category, data, addToCart }) {
 
-    const [ likes, setLikes] = useState({});
+  const [likes, setLikes] = useState({});
 
-    const handleLike = (itemId) => {
-        setLikes((prevLikes) => ({
-            ...prevLikes,
-            [itemId] : (prevLikes[itemId] || 0) + 1,
-        }));
-    };
+  const handleLike = (itemId) => {
+    setLikes((prevLikes) => ({
+      ...prevLikes,
+      [itemId]: (prevLikes[itemId] || 0) + 1,
+    }));
+  };
 
     return (
         <div>
@@ -20,9 +20,19 @@ function Category ({ category, data, addToCart }) {
                         <div>
                             <img src={item.imgUrl} alt={item.name} />
                         </div>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+                        <div>
+              <p>Name: {item.name}</p>
+              <p>Price: Ksh{item.price}</p>
+              <p>Details: {item.details}</p>
+              <button onClick={() => addToCart({ id: itemId, ...item })}>Add to Cart</button>
+              <button onClick={() => handleLike(itemId)}>Like</button>
+              <p>Likes: {likes[itemId] || 0}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Category;
