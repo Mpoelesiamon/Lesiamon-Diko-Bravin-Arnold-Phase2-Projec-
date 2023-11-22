895daefe28719e826bfed4a83bb13ffd860fd309
Bravin <onsasebravin853@gmail.com>
2023-11-22 14:27:33 +0300

    Category

--- src/components/Category.js
+++ src/components/Category.js
@@ -11,28 +11,18 @@ function Category({ category, data, addToCart }) {
     }));
   };
 
-  return (
-    <div>
-      <h2>{category}</h2>
-      <ul>
-        {Object.entries(data).map(([itemId, item]) => (
-          <li key={itemId}>
-            <div>
-              <img src={item.imgUrl} alt={item.name} />
-            </div>
-            <div>
-              <p>Name: {item.name}</p>
-              <p>Price: Ksh{item.price}</p>
-              <p>Details: {item.details}</p>
-              <button onClick={() => addToCart({ id: itemId, ...item })}>Add to Cart</button>
-              <button onClick={() => handleLike(itemId)}>Like</button>
-              <p>Likes: {likes[itemId] || 0}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h2>{category}</h2>
+            <ul>
+                {Object.entries(data).map(([itemId, item]) => (
+                    <li key={itemId}>
+                        <div>
+                            <img src={item.imgUrl} alt={item.name} />
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
-
-export default Category;
