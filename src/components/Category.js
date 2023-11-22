668da8f818ce7089d68fbff4980f63174b4bf749
Bravin <onsasebravin853@gmail.com>
2023-11-22 11:43:15 +0300

    const handleLike

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,4 +3,6 @@ import React, { useState } from "react";
 function Category ({ category, data, addToCart }) {
 
     const [ likes, setLikes] = useState({});
+
+    const handleLike = (itemId) => {}
 }
